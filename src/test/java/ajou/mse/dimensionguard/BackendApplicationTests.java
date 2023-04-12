df69436a37937da1b33f9044950cb583b51d0a3b
Wo-ogie <siwall0105@gmail.com>
2023-04-12 18:05:25 +0900

    프로젝트 이름 변경

--- src/test/java/ajou/mse/dimensionguard/BackendApplicationTests.java
+++ src/test/java/ajou/mse/dimensionguard/BackendApplicationTests.java
@@ -1,13 +1,13 @@
-package ajou.mse.dream;
+package ajou.mse.dimensionguard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DreamApplicationTests {
+class BackendApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
