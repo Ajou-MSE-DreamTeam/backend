6c4d8d5eb1870b02bc91139d8e1ae6b432ab96f2
Wo-ogie <siwall0105@gmail.com>
2023-04-15 18:07:43 +0900

    feat: #3 회원(`Member`) domain 및 repository layer 구현

---
+++ src/main/java/ajou/mse/dimensionguard/domain/Member.java
@@ -0,0 +1,44 @@
+package ajou.mse.dimensionguard.domain;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id")
+    private Integer id;
+
+    @Column(unique = true, nullable = false)
+    private String accountId;
+
+    @Column(nullable = false)
+    private String password;
+
+    public static Member of(String accountId, String password) {
+        return of(null, accountId, password);
+    }
+
+    public static Member of(Integer id, String accountId, String password) {
+        return Member.builder()
+                .id(id)
+                .accountId(accountId)
+                .password(password)
+                .build();
+    }
+
+    @Builder(access = AccessLevel.PRIVATE)
+    private Member(Integer id, String accountId, String password) {
+        this.id = id;
+        this.accountId = accountId;
+        this.password = password;
+    }
+}
